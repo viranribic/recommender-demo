1f1d1c392b89969c155d6f9014c372bb6e97002b
vribic <viran.ribic@gmail.com>
2018-03-23 16:09:18 +0100

    Added cors support.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //Install express server
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
