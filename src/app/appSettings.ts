d9a6a2f46afd25dc19817fd8788687b899e6ed95
vribic <viran.ribic@gmail.com>
2018-04-10 15:43:07 +0200

    Changed backend domain

--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
    public static APP_URL = 'https://recommender-demo.herokuapp.com/';
-   public static API_URL = 'https://vribic-heroku-test.herokuapp.com/';
+   public static API_URL = 'https://recommender-demo-api.herokuapp.com/';
    public static STORAGE_URL = 'https://test-bucket-vribic.s3.amazonaws.com/';
    //public static APP_URL = 'http://localhost:4200/';
    //public static API_URL = 'http://localhost:8000/';
